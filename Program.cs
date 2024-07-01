c376470b89b9c0e24335956e011dca8686f9db0d
SrAxel <Thesense@outlook.es>
2024-07-01 20:20:58 -0300

    Editar y crear de Alumnos y Carreras Funcionando

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorVsCode;
+using BlazorVsCode.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+var urlApi = builder.Configuration.GetValue<string>("urlApi");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri (urlApi) });
+builder.Services.AddScoped<IAlumnoService, AlumnoService>(); 
 
 await builder.Build().RunAsync();
---
+++ Services/AlumnoService.cs
@@ -0,0 +1,28 @@
+﻿using BlazorVsCode.Models;
+using System.Text.Json;
+
+namespace BlazorVsCode.Services
+{
+    public class AlumnoService : IAlumnoService
+    {
+        private readonly HttpClient client;
+        //private readonly JsonSerializerOptions options;
+
+        public AlumnoService(HttpClient client, JsonSerializerOptions options)
+        {
+            this.client = client;
+            //this.options = options;
+        }
+
+        public async Task<List<Alumno>?> Get()
+        {
+            var response = await client.GetAsync("apialumnos");
+            return await JsonSerializer.DeserializeAsync<List<Alumno>>(await response.Content.ReadAsStreamAsync());
+        }
+    }
+
+    public interface IAlumnoService
+    {
+        public Task<List<Alumno>?> Get();
+    }
+}
